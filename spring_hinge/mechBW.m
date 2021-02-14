9930f862d7e71616bc618f265e5e3a00e2bcf1dc
Sam Bunka <sambunka@gmail.com>
2021-02-13 20:07:40 -0800

    fixed bandwidth issue, now reflects preload in string

--- spring_hinge/mechBW.m
+++ spring_hinge/mechBW.m
@@ -39,10 +39,13 @@ tension_min = (theta0 - pi/2).*K./(L/2*sin(pi/4)); %N, min cable tension, at top
 tension_max = ((theta0).*K - m.*g.*L/2)./(L/2); %N, max cable tension, at bottom, supported by gravity. simulink
 motor_torque_max = tension_max*pulley_radius; % Nm, torque required to hold antenna full closed
 motor_torque_min = tension_min*pulley_radius;
-motor_current_max = motor_torque_max/T_max*I_max; % Amps, current required to hold antenna fully closed. Assumes that it will be proportional to ration between required torque and stall torque at 12V. 
+motor_current_max = motor_torque_max/T_max*I_max; % Amps, current required to hold antenna fully closed. Assumes that it will be proportional to ratio between required torque and stall torque at 12V. 
 motor_current_min = motor_torque_min/T_max*I_max;
 motor_power_max = V.*(motor_current_max./I_max).*motor_current_max; % W, power draw to hold position. Same as above, voltage scales down by proportion of required current and max current.
 motor_power_min = V.*(motor_current_max./I_max).*motor_current_min;
+if max(motor_current_max./I_max) > 1
+    print("One configuration exceeds motor performance");
+end
 
 %% Find excess lifiting ability
 % assume that lifted mass of cover increases linearly with elevation angle
@@ -75,6 +78,10 @@ motor_power_min = V.*(motor_current_max./I_max).*motor_current_min;
 
 theta = linspace(0, pi/2, 16);
 max_dust_weight = zeros(size(K, 1), size(K, 2), length(theta));
+danger_disturbance = (theta0 - pi/2).*K;
+slack_shock = danger_disturbance*(2*realI-L^2)/(realI*m*L); % m/s^2, shock perpendicular to antenna req'd to cause slack in line. page 136 of sam's logbook. might be wrong.
+
+
 for i = 1:length(theta)    
     max_dust_weight(:,:,i) = ((theta0 - theta(i)).*K- m*g*L/2.*cos(theta(i)))*pi/2*L./(sin(pi/2-theta(i)/2)*L^2/2*theta(i));
 end
@@ -91,13 +98,15 @@ s.EdgeColor = 'none';
 figure;
 % second plot shows the bandwidth for each spring combination, a simple
 % root(K/M) for a torsional setup. A higher bandwidth is better
-s = surf(theta0*180/pi, K, BW);
-xlabel("Undeformed Position, degrees");
-ylabel("Torque at full deformed, in-lbs");
-zlabel("Mechanical Bandwidth, Hertz");
-s.EdgeColor = 'none';
+% Feb 11, 2021: Actually not accurate due to preload in string, omitting
+% from output.
+% s = surf(theta0*180/pi, T, BW);
+% xlabel("Undeformed Position, degrees");
+% ylabel("Torque at full deformed, in-lbs");
+% zlabel("Mechanical Bandwidth, Hertz");
+% s.EdgeColor = 'none';
 
-% the thrid set of plots shows maximum dust cover weight for each spring
+% the third set of plots shows maximum dust cover weight for each spring
 % combination at different elevation angles. Only the 90 degree/vertical
 % plot is technically needed, since it offers the most conservative
 % estimate for the current parameters. However, I imagine if the geometry
@@ -109,10 +118,25 @@ s.EdgeColor = 'none';
 figure;
 for i = 1:length(theta)
     subplot(length(theta)/4, length(theta)/4, i)
-    s = surf(theta0*180/pi, K, max_dust_weight(:,:,i));
+    s = surf(theta0*180/pi, T, max_dust_weight(:,:,i));
     xlabel("Undeformed Position, degrees");
     ylabel("Torque at full deformed, in-lbs");
     zlabel("Max weight of dust cover, kg");
     title("Elevation angle = " + theta(i)*180/pi + " degrees")
     s.EdgeColor = 'none';
 end
+
+% fouth plot shows "danger disturbance torque" which causes cable to lose
+% tension. Causes vibrations, potential tangling. Also shows acceleration
+% of rover body req'd to cause loss of tension. 
+
+figure;
+s = surf(theta0*180/pi, T, danger_disturbance);
+hold on
+xlabel("Undeformed Position, degrees");
+ylabel("Torque at full deformed, in-lbs");
+zlabel("Disturbance torque (Nm)");
+title("Disturbance torque causing slack in line")
+s.EdgeColor = 'none';
+s = surf(theta0*180/pi, T, slack_shock);
+legend(["Disturbance torque", "Rover Body Shock (edges)"]);
