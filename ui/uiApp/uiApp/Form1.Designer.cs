840ffe4e639e8d15b736975972a35a4f6cdfa453
Sam Bunka <sambunka@gmail.com>
2021-03-09 01:02:07 -0800

    added UI elements, added most receiving code, working on sending code, nothing tested

--- ui/uiApp/uiApp/Form1.Designer.cs
+++ ui/uiApp/uiApp/Form1.Designer.cs
@@ -28,19 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
             this.portList = new System.Windows.Forms.ComboBox();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.elevBox = new System.Windows.Forms.TextBox();
+            this.aziBox = new System.Windows.Forms.TextBox();
+            this.aziSetpointBox = new System.Windows.Forms.TextBox();
+            this.elevSetpointBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.sendAziButton = new System.Windows.Forms.Button();
+            this.sendElevButton = new System.Windows.Forms.Button();
+            this.sendBothButton = new System.Windows.Forms.Button();
+            this.aziElevChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aziElevChart)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -64,29 +81,29 @@
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
-            // optionsToolStripMenuItem
-            // 
-            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
-            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
-            this.optionsToolStripMenuItem.Text = "Options";
-            // 
             // saveAsToolStripMenuItem
             // 
             this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
-            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.saveAsToolStripMenuItem.Text = "Save As";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.saveToolStripMenuItem.Text = "Save";
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
             this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
             // 
             // label1
             // 
@@ -106,20 +123,137 @@
             this.portList.TabIndex = 2;
             this.portList.SelectedIndexChanged += new System.EventHandler(this.portList_SelectedIndexChanged);
             // 
-            // trackBar1
+            // label2
             // 
-            this.trackBar1.Location = new System.Drawing.Point(20, 73);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(104, 45);
-            this.trackBar1.TabIndex = 3;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Azimuth";
             // 
-            // trackBar2
+            // label3
             // 
-            this.trackBar2.Location = new System.Drawing.Point(204, 27);
-            this.trackBar2.Name = "trackBar2";
-            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
-            this.trackBar2.Size = new System.Drawing.Size(45, 72);
-            this.trackBar2.TabIndex = 4;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Elevation";
+            // 
+            // elevBox
+            // 
+            this.elevBox.Location = new System.Drawing.Point(132, 77);
+            this.elevBox.Name = "elevBox";
+            this.elevBox.Size = new System.Drawing.Size(62, 20);
+            this.elevBox.TabIndex = 7;
+            // 
+            // aziBox
+            // 
+            this.aziBox.Location = new System.Drawing.Point(62, 77);
+            this.aziBox.Name = "aziBox";
+            this.aziBox.Size = new System.Drawing.Size(64, 20);
+            this.aziBox.TabIndex = 9;
+            // 
+            // aziSetpointBox
+            // 
+            this.aziSetpointBox.Location = new System.Drawing.Point(62, 104);
+            this.aziSetpointBox.Name = "aziSetpointBox";
+            this.aziSetpointBox.Size = new System.Drawing.Size(64, 20);
+            this.aziSetpointBox.TabIndex = 10;
+            // 
+            // elevSetpointBox
+            // 
+            this.elevSetpointBox.Location = new System.Drawing.Point(132, 104);
+            this.elevSetpointBox.Name = "elevSetpointBox";
+            this.elevSetpointBox.Size = new System.Drawing.Size(62, 20);
+            this.elevSetpointBox.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Actual";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Setpoint";
+            // 
+            // sendAziButton
+            // 
+            this.sendAziButton.Location = new System.Drawing.Point(62, 130);
+            this.sendAziButton.Name = "sendAziButton";
+            this.sendAziButton.Size = new System.Drawing.Size(64, 34);
+            this.sendAziButton.TabIndex = 12;
+            this.sendAziButton.Text = "Send Azimuth";
+            this.sendAziButton.UseVisualStyleBackColor = true;
+            this.sendAziButton.Click += new System.EventHandler(this.sendAziButton_Click);
+            // 
+            // sendElevButton
+            // 
+            this.sendElevButton.Location = new System.Drawing.Point(132, 130);
+            this.sendElevButton.Name = "sendElevButton";
+            this.sendElevButton.Size = new System.Drawing.Size(62, 34);
+            this.sendElevButton.TabIndex = 12;
+            this.sendElevButton.Text = "Send Elevation";
+            this.sendElevButton.UseVisualStyleBackColor = true;
+            // 
+            // sendBothButton
+            // 
+            this.sendBothButton.Location = new System.Drawing.Point(62, 171);
+            this.sendBothButton.Name = "sendBothButton";
+            this.sendBothButton.Size = new System.Drawing.Size(132, 23);
+            this.sendBothButton.TabIndex = 13;
+            this.sendBothButton.Text = "Send Both";
+            this.sendBothButton.UseVisualStyleBackColor = true;
+            // 
+            // aziElevChart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.aziElevChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.aziElevChart.Legends.Add(legend1);
+            this.aziElevChart.Location = new System.Drawing.Point(12, 200);
+            this.aziElevChart.Name = "aziElevChart";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Azimuth";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Elevation";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series3.Legend = "Legend1";
+            series3.Name = "Azimuth Setpoint";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series4.Legend = "Legend1";
+            series4.Name = "Elevation Setpoint";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "CW Limit Switch";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "CCW Limit Switch";
+            this.aziElevChart.Series.Add(series1);
+            this.aziElevChart.Series.Add(series2);
+            this.aziElevChart.Series.Add(series3);
+            this.aziElevChart.Series.Add(series4);
+            this.aziElevChart.Series.Add(series5);
+            this.aziElevChart.Series.Add(series6);
+            this.aziElevChart.Size = new System.Drawing.Size(776, 238);
+            this.aziElevChart.TabIndex = 14;
+            this.aziElevChart.Text = "Chart1";
             // 
             // Form1
             // 
@@ -127,8 +261,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.trackBar2);
-            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.aziElevChart);
+            this.Controls.Add(this.sendBothButton);
+            this.Controls.Add(this.sendElevButton);
+            this.Controls.Add(this.sendAziButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.elevSetpointBox);
+            this.Controls.Add(this.aziSetpointBox);
+            this.Controls.Add(this.aziBox);
+            this.Controls.Add(this.elevBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.portList);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
@@ -137,8 +281,7 @@
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aziElevChart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,8 +297,18 @@
         private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox portList;
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox elevBox;
+        private System.Windows.Forms.TextBox aziBox;
+        private System.Windows.Forms.TextBox aziSetpointBox;
+        private System.Windows.Forms.TextBox elevSetpointBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button sendAziButton;
+        private System.Windows.Forms.Button sendElevButton;
+        private System.Windows.Forms.Button sendBothButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart aziElevChart;
     }
 }
 
--- ui/uiApp/uiApp/Form1.cs
+++ ui/uiApp/uiApp/Form1.cs
@@ -22,25 +22,70 @@ namespace uiApp
         System.Windows.Forms.Timer portTimer;
         ConcurrentQueue<byte> dataReceived;
         ConcurrentQueue<byte> dataToSend;
+        List<packet> inPackets;
+        List<packet> outPackets;
+        Int32 inPacketCount = 0;
+        Int32 outPacketCount = 0;
+
+        public const int IN_PACKET_LEN = 9;
+        public const int OUT_PACKET_LEN = 9;
+        public const double AZI_DEG_PER_BIT = 0.04;
+        public const double ELEV_DEG_PER_BIT = 0.057;
 
         public Form1()
         {
             InitializeComponent();
 
+            port = new System.IO.Ports.SerialPort
+            {
+                BaudRate = 115200,
+                DataBits = 8,
+                Handshake = Handshake.None,
+                StopBits = StopBits.One
+            };
+            port.DataReceived += dataRecieved;
+
+            PortListUpdate();
+
             dataReceived = new ConcurrentQueue<byte>();
             dataToSend = new ConcurrentQueue<byte>();
+            inPackets = new List<packet>();
+            inPackets[0] = new packet(IN_PACKET_LEN); 
+            outPackets = new List<packet>();
+
 
             portTimer = new System.Windows.Forms.Timer();
             portTimer.Interval = 100;
             portTimer.Tick += portTimerTick;
             portTimer.Start();
-
-
         }
 
         private void portTimerTick(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            if (port.IsOpen == false)
+                return;
+
+            //ConcurrentQueue<int> anotherQueue = new System.Collections.Concurrent.ConcurrentQueue<int>();
+            //anotherQueue.Enqueue(5);
+            //anotherQueue.Enqueue(5);
+            //anotherQueue.Enqueue(5);
+            //anotherQueue.Enqueue(5);
+            //int tempp;
+            //bool resultssts = anotherQueue.TryDequeue(out tempp);
+            //resultssts = anotherQueue.TryDequeue(out tempp);
+            //resultssts = anotherQueue.TryDequeue(out tempp);
+            //resultssts = anotherQueue.TryDequeue(out tempp);
+
+            inPackets[inPacketCount].unpack(dataReceived);
+
+            if(inPackets[inPacketCount].index == inPackets[inPacketCount].PACKET_LENGTH)
+            {
+                aziBox.Text = inPackets[inPacketCount].azi.ToString();
+                elevBox.Text = inPackets[inPacketCount].elev.ToString();
+
+                inPacketCount++;
+                inPackets[inPacketCount] = new packet(IN_PACKET_LEN);
+            }
         }
 
         private void chart1_Click(object sender, EventArgs e)
@@ -74,5 +119,94 @@ namespace uiApp
         {
 
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sendAziButton_Click(object sender, EventArgs e)
+        {
+            outPackets[outPacketCount] = new packet(OUT_PACKET_LEN);
+            outPackets[outPacketCount].setAzi(double.Parse(aziSetpointBox.Text));
+            outPackets[outPacketCount].pack();
+        }
+    }
+
+    class packet
+    {
+        public byte[] data;
+        public int index;
+        public int PACKET_LENGTH;
+        public double elev, azi;
+        public packet(int packLen)
+        {
+            PACKET_LENGTH = packLen);
+            data = new byte[PACKET_LENGTH];
+            index = 0;
+        }
+
+        public void unpack(ConcurrentQueue<byte> bytes)
+        {
+            byte temp;
+            while (bytes.TryDequeue(out temp) == true)
+            {
+                if (temp == 255)
+                {
+                    index = 0;
+                    data[index] = (byte)temp;
+                    index++;
+                }
+                else if (1 <= index && index < PACKET_LENGTH)
+                {
+                    data[index] = (byte)temp;
+                    index++;
+
+                    //if packet is now full (entered last byte)
+                    if (index == PACKET_LENGTH)
+                    {
+                        index = 0;
+                        parseElev();
+                        parseAzi();
+                        break; //break even if more to dequeue
+                    }
+                }
+            }
+        }
+
+        public void pack(int cmd, double elev, double azi, int speed)
+        {
+            data[0] = 255;
+            data[1] = (byte)cmd;
+            
+
+        }
+
+        private void parseElev()
+        {
+            elev = (double)((int)data[1] << 8 + data[2]) * Form1.ELEV_DEG_PER_BIT;
+        }
+
+        private void parseAzi()
+        {
+            azi = (double)((int)data[3] << 8 + data[4]) * Form1.AZI_DEG_PER_BIT;
+        }
+        public void setElev(double elevIn)
+        {
+            elev = elevIn;
+            int elevInt = (int)(elevIn / Form1.ELEV_DEG_PER_BIT);
+            data[1] = (byte) (elevInt >> 8);
+            data[2] = (byte)elevInt;
+        }
+
+        public void setAzi(double aziIn)
+        {
+            azi = aziIn;
+            int aziInt = (int)(aziIn / Form1.ELEV_DEG_PER_BIT);
+            data[3] = (byte)(aziInt >> 8);
+            data[4] = (byte)aziInt;
+        }
     }
 }
+
+
